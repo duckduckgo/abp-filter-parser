7204b204a34cf15d113bb6dbddea2aff581fd6c3
jdorweiler <jason@duckduckgo.com>
2018-02-07 14:13:42 -0700

    fix typo

--- src/abp-filter-parser.js
+++ src/abp-filter-parser.js
@@ -124,7 +124,7 @@ export function parseOptions(input) {
 
       // unsupported options: this can include request types since they
       // fall through the if(elementTypeMaskMap) above
-      if (!(supportedOptions[option] && elementTypeMaskMap.has(option)) {
+      if (!(supportedOptions[option] && elementTypeMaskMap.has(option))) {
           if (!output.unsupported) {
               output.unsupported = []
           }
