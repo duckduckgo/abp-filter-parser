39aeed64f5cd77283b96d7d41a68a7071727378b
jdorweiler <jason@duckduckgo.com>
2018-02-09 14:42:27 -0700

    add list name for debugging

--- src/abp-filter-parser.js
+++ src/abp-filter-parser.js
@@ -551,7 +551,7 @@ function hasMatchingFilters(filterList, parsedFilterData, input, contextParams, 
     // debug logging for matched filters. To turn this on run this in the extension console
     // abp.setFilterDebugging(true)
     if (filterDebugging) {
-        console.log("FOUND FILTER\n" + "Request: " + input + "\n" + "Filter: " + JSON.stringify(foundFilter))
+        console.log("Matched Filter\n" + "List name: " + contextParams.listName "\n" + "Request: " + input + "\n" + "Filter: " + JSON.stringify(foundFilter))
     }
 
     // increment the count of matches
